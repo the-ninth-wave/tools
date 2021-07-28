ad13f0f1fdea9839f445d7a9b425e1d9029e7678
the-ninth-wave <julian.thomas.gold@gmail.com>
2021-07-28 13:01:20 -0500

    Update tools_test.py

--- tools_test.py
+++ tools_test.py
@@ -559,13 +559,13 @@ import transforms as T
 """
 
 
-def get_transform(train):
-    transforms = []
+# def get_transform(train):
+    # transforms = []
     # converts the image, a PIL image, into a PyTorch Tensor
-    transforms.append(T.ToTensor())
-    if train:
+    # transforms.append(T.ToTensor())
+    # if train:
         # during training, randomly flip the training images
         # and ground truth for data augmentation
         # transforms.append(T.RandomHorizontalFlip(0.5))
-    return T.Compose(transforms)
+    # return T.Compose(transforms)
    
