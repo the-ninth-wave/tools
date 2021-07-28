1613d505cf1c634be9b835fe558ed587f84bcf83
the-ninth-wave <julian.thomas.gold@gmail.com>
2021-07-27 20:02:02 -0500

    update

--- tools_test.py
+++ tools_test.py
@@ -3,14 +3,19 @@
 #
 import os
 import re
-from bs4 import BeautifulSoup # Beautiful Soup
 from pathlib import Path
+from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
+import collections, itertools
 
-def test():
-    print("Hello world")
-    
+
+####################################################
+####################################################
 """
+                               Part I
+                               
+                   ( displaying the local directory )
+
 
 Modifying code from: 
 
@@ -18,15 +23,6 @@ https://stackoverflow.com/
 questions/9727673/list-directory-tree-structure-in-python
 
 """
-###
-## req. imports:
-#
-# from pathlib import Path
-# import re
-# import xml.etree.ElementTree as ET
-
-
-
 """
 The following regex describes the pattern 
 we want to use as a criterion for omitting
@@ -34,22 +30,17 @@ the listing
 """
 regex_raw_all = r"[/][.]|/_|Icon"
 regex_all = re.compile(regex_raw_all)
-#
-##
-#
+
+#key symbols in tree diagram display
+element_prefix_middle = '├──'
+element_prefix_last = '└──'
+parent_prefix_middle = '    '
+parent_prefix_last = '│   '
+
 class DisplayablePath(object):
-    
-    # key symbols
-    display_filename_prefix_middle = '├──'
-    display_filename_prefix_last = '└──'
-    display_parent_prefix_middle = '    '
-    display_parent_prefix_last = '│   '
 
     # initialize
-    def __init__(self, 
-                 path, 
-                 parent_path, 
-                 is_last):
+    def __init__(self, path, parent_path, is_last):
         
         self.path = Path(str(path))
         self.parent = parent_path
@@ -59,9 +50,6 @@ class DisplayablePath(object):
         else:
             self.depth = 0
     
-    """
-    displayname
-    """
     @property
     def displayname(self):
         
@@ -71,13 +59,12 @@ class DisplayablePath(object):
         else:
             return self.path.name
 
-    """
-    make_tree 
-    
-        is a recursive method for 
-        displaying an ASCII tree diagram
-        
-    """
+
+    #    make_tree 
+    #
+    # is a recursive method for 
+    # displaying an ASCII tree diagram  
+    #
     @classmethod
     def make_tree(cls, 
                   root, 
@@ -85,8 +72,7 @@ class DisplayablePath(object):
                   is_last=False, 
                   criteria=None):
         
-        # initializing a 'class object' for root
-        root = Path(str(root))
+        root = Path(str(root))   #  init class object for root
         displayable_root = cls(root, parent, is_last)
         yield displayable_root        #   yield functions as "return"
         
@@ -99,38 +85,21 @@ class DisplayablePath(object):
         
         We set the class default to handle local files below.
         """
-        #
-        # criteria for display
-        #
         criteria = criteria or cls._default_criteria
         
-        #
+        
         # collect children of the given current node,
         # filtered by criteria
-        #
         children = sorted(list(path
                                for path in root.iterdir()
                                if criteria(path)),
                           key=lambda s: str(s).lower())
         
-        #
-        # count sets tree depth to one.
+        # count = 1 effectively sets tree depth to one.
         count = 1
         
-        #
-        # we perform the recursion for each element of children
-        #
-        """
-        
-        my understanding of the recursive step is still hazy, 
-        especially regarding the distinct types of the "returns" 
-        in the two cases below
-        
-        """
         for path in children:
-            
             is_last = count == len(children)
-            
             if path.is_dir():
                 yield from cls.make_tree(path,
                                          parent=displayable_root,
@@ -140,15 +109,13 @@ class DisplayablePath(object):
                 yield cls(path, displayable_root, is_last)
             count += 1
 
-    """
-    _default_criteria
-    
-        the default criterion is meant to apply
-        to the MacOS local env, so that no
-        superfluous information is displayed in the tree. 
-        
-        
-    """
+
+    #     _default_criteria
+    #
+    # the default criterion is meant to apply
+    # to the MacOS local env, so that no
+    # superfluous information is displayed in the tree. 
+    #
     @classmethod
     def _default_criteria(cls, path):
         
@@ -159,100 +126,94 @@ class DisplayablePath(object):
         return boo
         
 
-    """
-    displayable
-    
-        key symbols
-    
-        display_filename_prefix_middle = '├──'
-        display_filename_prefix_last = '└──'
-        display_parent_prefix_middle = '    '
-        display_parent_prefix_last = '│   '
-        
-    """
-    @property # ??
+    @property 
     def displayable(self):
         
-        # this indicates self is the root
-        if self.parent is None:
+        
+        if self.parent is None: #    'True' indicates self is root
             return self.displayname #    the return skips the rest
         
-        ###
-        ## to see this, self must not be the root 
-        # however, this is the part of the display that 
-        # links to the rest of the tree
-        #
-        _filename_prefix = (self.display_filename_prefix_last # └──
+        """
+        to see what follows this, self must not be the root 
+        """
+        
+        _filename_prefix = (self.element_prefix_last # └──
                             if 
                             self.is_last
                             else 
-                            self.display_filename_prefix_middle) # ├──
+                            self.element_prefix_middle) # ├──
         
-        ###
-        ## we combine the prefix with the file name into
-        # a _single_ string
-        #
+        # we combine the prefix with the file name
         parts = ['{!s} {!s}'.format(_filename_prefix,
                                     self.displayname)]
         
-        ###
-        ## record self's parent
-        #
-        parent = self.parent
+        parent = self.parent #    record self's parent
         
-        ### 
-        ## while parent is not root, and 
-        #
+
         while parent and parent.parent is not None:
-            parts.append(self.display_parent_prefix_middle # (space)
+            parts.append(self.parent_prefix_middle # (space)
                          if parent.is_last
-                         else self.display_parent_prefix_last) # │   
+                         else self.parent_prefix_last) # │   
             parent = parent.parent # this step climbs the tree
             
-            """
-            Because we append to "parts"
-            as we climb the tree, we must reverse 
-            the sequence built by the climbing
-            of the tree
-            """
-
+        
+        # Because we append to "parts" as we climb the tree, 
+        # we must reverse the sequence built by the climbing          
         return ''.join(reversed(parts))
 
 """
-Above code from:
-https://stackoverflow.com/
-questions/9727673/list-directory-tree-structure-in-python
-"""
-#
-##
-#
 
+    disp() the the central 'output' of Part I,
+    the point is to visualize the local directory as
+    a tree
+
+"""
 def disp():
     paths = DisplayablePath.make_tree( os.getcwd() )
     for path in paths:
         print(path.displayable)
 
-
-def display_xml(root):
-    for i in root.iter():
-        s = displayable_Element( tree, i, i.attrib['is_last'])
-        print(s)
         
-## returns attributes as a formatted string
-# to be printed, as a function of element
-import json
+        
+        
+        
+        
+        
+        
+####################################################
+####################################################
+"""
+                               Part II
+                               
+        ( analogous methods for xml files, using ElementTree )
 
-def att_string(element):
-    return(json.dumps(element.attrib))
+The methods display_xml and display_xml_text below are like disp()
+for objects in ElementTree. 
 
-def display_xml_text(root):
-    for element in root.iter():
+Sources:
+
+* https://stackoverflow.com/questions/17275524/xml-etree-elementtree-get-node-depth
+
+* https://stackoverflow.com/questions/323750/how-to-access-the-previous-next-element-in-a-for-loop
+
+* https://stackoverflow.com/questions/2170610/access-elementtree-node-parent-node
+
+
+"""
+
+def display_xml(path):
     
-        # tree
-        s = displayable_Element( tree, element, element.attrib['is_last'])
+    tree = ET.parse(path)
+    root = tree.getroot()
     
+    for i in root.iter():
+        s = displayable_Element(tree, i, i.attrib['is_last'])
         print(s + ' :  ' + element.text)
         
+"""
+                       Helper functions 
+"""
+
 def depth_iter(element, tag = None):
     stack = []
     stack.append(iter([element]))
@@ -265,89 +226,44 @@ def depth_iter(element, tag = None):
             stack.append(iter(e))
             if tag == None or e.tag == tag:
                 yield (e, len(stack) - 1)
-"""
-One can call depth_iter by
 
-for i in depth_iter(root):
-    print(i[0].tag + '\t' + str(i[1]) )
-
-https://stackoverflow.com/questions/17275524/xml-etree-elementtree-get-node-depth
-
-"""
 
 def depth_record(tree):
     
-    tree = tree
-    
     root = tree.getroot()
     
-    for depth_element in depth_iter(root):
-        
-        # the element itself
-        depth_element[0].set( 'depth' , str( depth_element[1]) )
+    for j in depth_iter(root):
+        j[0].set('depth', str(j[1]))
         
     return(tree)
 
-"""
-To test this works:
-
-for i in root.iter():
-    print(i.attrib['depth'])
-
-Once we have recorded depth, we can use the depth of consecutive elements to determine which are "last"
-
-Rmk
-
-    https://stackoverflow.com/questions/323750/how-to-access-the-previous-next-element-in-a-for-loop
-
-has an answer including a utility function that gives a sliding window on an iterator. I couldn't get this to work, but seems worth knowing about.
-
-"""
-
-import collections, itertools
 
 
 def last_record(tree):
     
-    # store tree
-    tree = tree
-    
-    # store root
     root = tree.getroot()
     
-    # this vector records the depth of each element,
-    # in the order the elements are visited by iteration,
-    # so "depth-first"
-    depth_v = []
+    # records depth of each element, as they are 
+    # visited by depth-first search
+    depth_v = [] 
     
     for element in root.iter():
         depth_v += [ int(element.attrib['depth'])]
         
-    
     last_v = []
     
-    # we are looking one ahead inside the for loop
-    # so we use the range
-    """
-     0  to  len( depth_v ) - 1
-    """
-    # in order to not plug in an invalid index
+    # looking index ahead inside the for loop
+    # so we use the range   [ 0 , len( depth_v ) - 1 ]
     for ind in range(0,len(depth_v)-1):
         boo = False
         if depth_v[ind] > depth_v[ind+1]:
             boo = True
         
         last_v += [ boo ]
-        
-    # the last item listed should
-    # always have last status set to true.
+       
+    last_v += [ True ] # last item always has last status set to true
     
-    last_v += [ True ]
-    
-    # now we annotate each element of the tree with the 
-    # "is_last" attribute information
-    
-    count = 0
+    count = 0 # annotate each tree element with the is_last attrib
     
     for element in root.iter():
         element.set( 'is_last' , str( last_v[count] ) )
@@ -355,36 +271,20 @@ def last_record(tree):
             
     return(tree)
 
-"""
-From
-
-    https://stackoverflow.com/questions/2170610/access-elementtree-node-parent-node
-
-I take code for the parent_map; this information needed in the method below.
-
-"""
-
-# This should just return the parent of the given element
+#   parent()
+#
+# creates a dictionary with ( element, parent ) as 
+# ( key, value ) pairs. 
+#
 def parent(tree, element):
-    # from elementTree spellbook;
-    # this creates a dictionary of the form
-    """
-        key     :   value
-        element :   parent
-    
-    """
-    parent_map = {c:p for p in tree.iter() for c in p}
-    ###
-    ## to use, will collect these pairs as an array
-    #
-    parent_list = []
 
+    parent_map = {c:p for p in tree.iter() for c in p}
+
+    parent_list = []
     for key, value in parent_map.items():
-        parent_list += [[ key, value]]
-        
+        parent_list += [[ key, value]]  
     
-    # get index
-    ind = 0
+    ind = 0 # get index
     count = 0
     for el in root.iter():
         if el == element:
@@ -394,95 +294,81 @@ def parent(tree, element):
     return(parent_list[ind-1][1])
 
 """
-`displayable_Element`
-
-Instead of trying to pass the data of an xml file
-to the class created above, mediated by an ElementTree
-object, let's try to repurpose the 
-"displayable" method to operate on Element objects.
-
-We also pass the tree so that we can detect
-when the root is passed, via tag. 
-
-This assumes the tag of the root is unique. 
-
-Finally, we pass a Boolean indicating whether the element
-is last among children of its parent. 
-
-_key symbols_
+    displayable_Element
     
-    element_prefix_middle = '├──'
-    element_prefix_last = '└──'
-    parent_prefix_middle = '    '
-    parent_prefix_last = '│   '
+This is a standalone method in part II meant to copy the functionality
+of the class property displayable() for DisplayablePath objects. 
 
-Note that we assume the tree has been annotated so that
+We do this here without creating a new class for "displayable 
+element trees."
+
+
+We pass the tree so that we can detect
+when the root is passed, via tag - this assumes 
+the tag of the root is unique. 
+
+We assume the tree has been annotated so that
 
     * each Element has an attribute 'depth'
     * each Element has an attribute 'is_last'
     
-both attributes are strings, but on use are cast as `int` and `Bool` respectively. 
+both attributes are strings, but on use are respectively recast as int and bool. 
 
-"""
+To ensure that the xml file to be read in is properly
+annotated, we call the next function to do preprocessing.
 
-def preprocess():
-    # record depth attribute
+When playing around, we used a fixed string for the path to the xml file, and so preprocess had no arguments. We had set
+
     tree = ET.parse('dcd_clips/labels_xml/1.xml')
     root = tree.getroot()
-    tree = depth_record(tree)
-    # record is_last attribute
-    tree = last_record(tree)
+
+then called
+
+    preprocess()
+
+below preprocess must take in a path, and 
+instead of passing the tree defined just above through ET.parse,
+we pass the general path and apply the ET.parsse method inside 
+the displayable_Element method
+"""
+
+def preprocess(path): # e.g.,  path = 'dcd_clips/labels_xml/1.xml'
+    
+    tree = ET.parse(path)
+    root = tree.getroot()
+    tree = depth_record(tree) # record depth attribute
+    tree = last_record(tree) # record is_last attribute
+    root = tree.getroot()
+    tree.write(path)
+
+# tree = ET.parse('dcd_clips/labels_xml/1.xml')
+# root = tree.getroot()
+# preprocess()
+
+def displayable_Element(path, element, is_last):
+    
+    boo = bool(is_last)
+    tree = ET.parse(path)
     root = tree.getroot()
     
-    tree.write('dcd_clips/labels_xml/1.xml')
+    preprocess(path)
 
-tree = ET.parse('dcd_clips/labels_xml/1.xml')
-root = tree.getroot()
-#preprocess()
-
-def displayable_Element( tree, element, is_last ):
-    boo = bool(is_last)
-    
-    # key symbols in tree diagram display
-    element_prefix_middle = '├──'
-    element_prefix_last = '└──'
-    parent_prefix_middle = '    '
-    parent_prefix_last = '│   '
-    
     # the text to be displayed 
     display = element.tag 
         
-    # this indicates the element passed is the root
-    if tree.getroot().tag == element.tag:
+    if root.tag == element.tag: #   if element is root
         return( display ) 
-        #    the above return skips the rest
-        #    
-        #    The string in the return plays role of
-        #    "self.displayname" from original method
-        
-    ###
-    ## to see this, self must not be the root 
-    # however, this is the part of the display that 
-    # links to the rest of the tree
-    #
+
     prefix = (element_prefix_last # └──
                         if 
                         boo
                         else 
                         element_prefix_middle) # ├──
-        
-    ###
-    ## combine the diagram prefix with display text
-    #
+    
     parts = ['{!s} {!s}'.format( prefix, display )]
-        
-    ###
-    ## get the parent
-    #
+    
     ancestor = element
 
-    
-    
     while int(ancestor.attrib['depth']) > 1:
         boo3 = bool(ancestor.attrib['is_last'])
         if boo3:
