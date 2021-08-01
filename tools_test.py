c6f3c450b2c6054ee3438c5c6e0c7ace122d7738
the-ninth-wave <julian.thomas.gold@gmail.com>
2021-08-01 15:53:15 -0500

    Update tools_test.py

--- tools_test.py
+++ tools_test.py
@@ -27,12 +27,14 @@ import cv2
 # import utils
 # import transforms as T
 
+"""
 from zipfile import ZipFile
 from shutil import copy
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from google.colab import auth
 from oauth2client.client import GoogleCredentials
+"""
 
 
 ####################################################
