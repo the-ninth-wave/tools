726f6b5cd5b034a1e7c05742f20e269ec9a5b4c5
the-ninth-wave <julian.thomas.gold@gmail.com>
2021-08-02 11:12:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from setuptools import setup, find_packages
 
 setup(name='tools_test', version = '0.1', packages = find_packages())
+setup(name='tools_test_drive', version = '0.1', packages = find_packages())
